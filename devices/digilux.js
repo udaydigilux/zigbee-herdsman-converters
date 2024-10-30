af393a90f16703ca5fd65f6e746257daf5846c05
dineshelegit <dineshelegit@gmail.com>
2024-10-30 16:20:38 +0530

    modified digilux.js

--- devices/digilux.js
+++ devices/digilux.js
@@ -12,11 +12,10 @@ const definitions = [
         description: 'Digilux panel',
         extend: [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7} }),
-            modernExtend_1.identify(),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '6', '7'] }),
             modernExtend_1.light({ endpointNames: ['3', '4' , '5'] }),
-            meta:{multiEndpoint: true},
         ],
+        meta:{multiEndpoint: true}
     },
     {
         zigbeeModel: ['DGL_ZB8S0D1F'],
@@ -24,10 +23,11 @@ const definitions = [
         vendor: 'Digilux',
         description: 'Digilux panel',
         extend: [
-            modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, , '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7 ,'8' : 8, '9':9} }),
+            modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7 ,'8' : 8, '9':9} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3','4','5','6','7','8'] }),
    modernExtend_1.light({ endpointNames: ['9'] }),
-   meta:{multiEndpoint: true},
+   ],
+   meta:{multiEndpoint: true}
 },
 {
         zigbeeModel: ['DGL_ZB6S0D1F'],
@@ -35,10 +35,11 @@ const definitions = [
         vendor: 'Digilux',
         description: 'Digilux panel',
         extend: [
-            modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, , '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7} }),
+            modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3','4','5','6'] }),
    modernExtend_1.light({ endpointNames: ['7'] }),
-   meta:{multiEndpoint: true},
+   ],
+   meta:{multiEndpoint: true}
 },
 {
         zigbeeModel: ['DGL_ZB4S0D1F'],
@@ -49,7 +50,8 @@ const definitions = [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3', '4'] }),
    modernExtend_1.light({ endpointNames: ['5'] }),
-   meta:{multiEndpoint: true},
+   ],
+   meta:{multiEndpoint: true}
 },
 {
         zigbeeModel: ['DGL_ZB2S1D1F'],
@@ -60,6 +62,7 @@ const definitions = [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2'] }),
    modernExtend_1.light({ endpointNames: ['3','4'] }),
+   ],
    meta:{multiEndpoint: true},
 },
 {
@@ -71,6 +74,7 @@ const definitions = [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2'] }),
    modernExtend_1.light({ endpointNames: ['3'] }),
+   ],
    meta:{multiEndpoint: true},
 },
 {
@@ -81,6 +85,7 @@ const definitions = [
         extend: [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5, '6': 6} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3','4','5','6'] }),
+            ],
    meta:{multiEndpoint: true},
 },
 {
@@ -91,6 +96,7 @@ const definitions = [
         extend: [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7 ,'8' : 8, '9':9 , '10':10} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3','4','5','6','7','8','9' ,'10'] }),
+            ],
    meta:{multiEndpoint: true},
 },
 {
@@ -101,6 +107,7 @@ const definitions = [
         extend: [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4, '5': 5, '6': 6, '7': 7 ,'8' : 8} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3','4','5','6','7','8'] }),
+            ],
    meta:{multiEndpoint: true},
 },
 {
@@ -111,6 +118,7 @@ const definitions = [
         extend: [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3 , '4': 4} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3','4'] }),
+            ],
    meta:{multiEndpoint: true},
 },
 {
@@ -121,6 +129,7 @@ const definitions = [
         extend: [
             modernExtend_1.deviceEndpoints({ endpoints: { '1': 1, '2': 2, '3' :3} }),
             modernExtend_1.onOff({ endpointNames: ['1', '2', '3'] }),
+            ],
    meta:{multiEndpoint: true},
 },
 {
@@ -136,3 +145,4 @@ const definitions = [
 ];
 exports.default = definitions;
 module.exports = definitions;
+
